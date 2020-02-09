22776854439c0e0a8342a602df05b0d5f22992da
Sad Pony <51142088+sadponyguerillaboy@users.noreply.github.com>
2020-02-09 15:11:37 -0500

    Add files via upload

---
+++ Source/EFI Patcher/EFI Patcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EFI Patcher
+//
+//  Created by NoOne on 2020-01-28.
+//  Copyright © 2020 None. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Source/EFI Patcher/EFI PatcherTests/EFI_PatcherTests.swift
@@ -0,0 +1,34 @@
+//
+//  EFI_PatcherTests.swift
+//  EFI PatcherTests
+//
+//  Created by NoOne on 2020-01-28.
+//  Copyright © 2020 None. All rights reserved.
+//
+
+import XCTest
+@testable import EFI_Patcher
+
+class EFI_PatcherTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
